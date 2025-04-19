ad753b143aa53add9edfafa5b282a30075cdbf74
Bruno Sales <salesdasilvabruno@gmail.com>
2025-04-19 12:23:12 -0300

    create Class Main

---
+++ src/br/com/alura/minhasmusicas/principal/Main.java
@@ -0,0 +1,32 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 500; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+    }
+}
